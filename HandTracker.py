14236d6a5acbf6430e5860112f8735d2c268784c
Monsif Hiani <monsif.hiani@etu.umontpellier.fr>
2025-01-15 09:10:01 +0100

    Classe HandTracker

--- HandTracker.py
+++ HandTracker.py
@@ -1,1 +1,34 @@
+import cv2
+import mediapipe as mp
+import numpy as np
 
+
+
+class HandTracker:
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands.Hands(
+            static_image_mode=False,
+            max_num_hands=2,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
+        )
+        self.mp_drawing = mp.solutions.drawing_utils
+
+    def get_hand_positions(self, frame, screen_height):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.mp_hands.process(frame_rgb)
+
+        hand_positions = {"Left": None, "Right": None}
+
+        if results.multi_hand_landmarks and results.multi_handedness:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                label = handedness.classification[0].label  # "Left" ou "Right"
+                index_tip_y = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP].y
+                hand_positions[label] = int(index_tip_y * screen_height)
+
+        return hand_positions
+
+    def draw_hands(self, frame, results):
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
